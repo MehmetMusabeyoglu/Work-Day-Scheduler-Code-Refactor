761c8f2dca2e9bb283158d82811f2fea26421377
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-06 21:40:14 -0700

    Added local storage saving feature.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Global variables
-var textArea09El = $("textArea09");
-var textArea10El = $("textArea10");
-var textArea11El = $("textArea11");
-var textArea12El = $("textArea12"); 
-var textArea13El = $("textArea13");
-var textArea14El = $("textArea14"); 
-var textArea15El = $("textArea15");
-var textArea16El = $("textArea16");
-var textArea17El = $("textArea17");
+var textArea9El = $("#textArea9");
+var textArea10El = $("#textArea10");
+var textArea11El = $("#textArea11");
+var textArea12El = $("#textArea12"); 
+var textArea13El = $("#textArea13");
+var textArea14El = $("#textArea14"); 
+var textArea15El = $("#textArea15");
+var textArea16El = $("#textArea16");
+var textArea17El = $("#textArea17");
 
-
+for(var i=9; i<=17; i++){
+    localStorage.setItem(i, ""); 
+}
 
 let currentTimeShown = function () {
     let currentTime = moment().format("Do MMM YYYY, ddd / HH:mm:ss");
@@ -137,6 +139,56 @@ function colorChange() {
 colorChange();
 
 
+// for (var i=9; i<=17; i++){
+//     $('#button' + i).on('click', function(){
+//         console.log($('#textArea'+i));
+//     })
+// }
 
+
+$('#button9').on('click',function(){
+    console.log('textArea9El');
+    localStorage.setItem(9, textArea9El.val());
+})
+
+$('#button10').on('click',function(){
+    console.log('textArea10El');
+    localStorage.setItem(10, textArea10El.val());
+})
+
+$('#button11').on('click',function(){
+    console.log('textArea11El');
+    localStorage.setItem(11, textArea11El.val());
+})
+
+$('#button12').on('click',function(){
+    console.log('textArea12El');
+    localStorage.setItem(12, textArea12El.val());
+})
+
+$('#button13').on('click',function(){
+    console.log('textArea13El');
+    localStorage.setItem(13, textAre13El.val());
+})
+
+$('#button14').on('click',function(){
+    console.log('textArea14El');
+    localStorage.setItem(14, textArea14El.val());
+})
+
+$('#button15').on('click',function(){
+    console.log('textArea15El');
+    localStorage.setItem(15, textArea15El.val());
+})
+
+$('#button16').on('click',function(){
+    console.log('textArea16El');
+    localStorage.setItem(16, textArea16El.val());
+})
+
+$('#button17').on('click',function(){
+    console.log('textArea17El');
+    localStorage.setItem(17, textArea17El.val());
+})
 
 
