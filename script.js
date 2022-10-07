1ae8cd9798f6c4f3a39dac4e7cdaaa38e1db5c64
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-06 18:12:46 -0700

    Added color change function to script.js file.

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
+// Variables
+
+
 let currentTimeShown = function () {
     let currentTime = moment().format("Do MMM YYYY, ddd / HH:mm:ss");
     $("#currentTime").text(currentTime);
 };
 currentTimeShown();
 setInterval(currentTimeShown, 1000);
+
+
+function colorChange() {
+    var currentHour = moment().hour();
+    // console.log(currentHour);
+
+    if (currentHour > 9) {
+        $('#time09').css('background-color', 'gray');
+    }
+    else if (currentHour < 9) {
+        $('#time09').css('background-color', 'green');
+    }
+    else {
+        $('#time09').css('background-color', 'red');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+colorChange();
+
+
+
+
 
